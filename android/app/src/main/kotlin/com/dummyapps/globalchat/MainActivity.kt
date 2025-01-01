a4c2ad1416366541e2a307128e641c19f7e241a5
subedisuraj1 <sr.subedi1@gmail.com>
2025-01-01 20:03:19 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/dummyapps/globalchat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dummyapps.globalchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:globalchat/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Firebase Working fine')),
+      ),
+    );
+  }
+}
